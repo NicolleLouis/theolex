2cd47273f81a371591f14511fc1c2de69bf26d9f
NicolleLouis <louisxnicolle@gmail.com>
2019-07-25 15:38:56 +0200

    Add db configuration

--- back/theolex/settings.py
+++ back/theolex/settings.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'theolex.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'theolex',
+        'USER': 'theolex',
+        'PASSWORD': 'theolex',
+        'HOST': '35.241.174.243',
+        'PORT': '5432',
     }
 }
 
