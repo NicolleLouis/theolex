c6316c4d5c4e1c21332500187d374823b5a697bf
NicolleLouis <louisxnicolle@gmail.com>
2019-07-26 12:48:52 +0200

    Revert to SQLite

--- back/back/settings.py
+++ back/back/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'zo!@0g3&nza+3!(-5ns4&2*30nxj!dfjzuy@h-_agjavkta!=5'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -69,21 +67,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'back.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'theolex',
-        'USER': 'theolex',
-        'PASSWORD': 'theolex',
-        'HOST': '35.241.174.243',
-        'PORT': '5432',
+        ######
+        # SQLITE
+        ######
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        ######
+        # POSTGRESQL
+        ######
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'theolex',
+        # 'USER': 'theolex',
+        # 'PASSWORD': 'theolex',
+        # 'HOST': '35.241.174.243',
+        # 'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -103,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -116,7 +119,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
