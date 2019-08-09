39e5dd2fdb4bacf46ee8793a9bd031c66ea5d96a
NicolleLouis <louisxnicolle@gmail.com>
2019-08-09 12:25:29 +0200

    Add migrations and admin view

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 local-proxy:
 	./cloud_sql_proxy -instances=amiable-dynamo-245709:europe-west1:theolex-database=tcp:5432
+
+python:
+	docker exec -it back_web_1 /bin/sh
--- back/api/admin.py
+++ back/api/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
+from django.contrib.auth.models import User, Group
 from .models import Decision, DecisionAdmin
 
 # Register your models here.
 admin.site.register(Decision, DecisionAdmin)
+
+# Clean useless model
+admin.site.unregister(User)
+admin.site.unregister(Group)
