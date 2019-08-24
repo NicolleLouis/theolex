4377b2831dcbd340da067a148e987d4ef17d3cac
NicolleLouis <louisxnicolle@gmail.com>
2019-08-24 13:43:21 +0200

    Rename api get_all_decisions to get_decisions

--- back/api/views.py
+++ back/api/views.py
@@ -2,11 +2,11 @@ import ast
 
 from django.http import JsonResponse
 
-from api.repository.decision_repository import fetch_all_decisions, get_decisions_with_filter
+from api.repository.decision_repository import get_decisions_with_filter
 from api.services.utils import transform_decision_list_to_json
 
 
-def get_all_decisions(request):
+def get_decisions(request):
     input_search_bar = request.GET.get('input_search_bar')
     # ToDo: input_search_bar match un des labels des jugements
     print(input_search_bar)
