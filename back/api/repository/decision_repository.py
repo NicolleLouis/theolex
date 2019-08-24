2ea8b18a47381b2d1a52e08163e06421ff0f743d
NicolleLouis <louisxnicolle@gmail.com>
2019-08-24 19:52:07 +0200

    Search field work on text search

--- back/api/repository/decision_repository.py
+++ back/api/repository/decision_repository.py
@@ -1,5 +1,3 @@
-import ast
-
 from api.models import Decision
 
 
@@ -7,12 +5,11 @@ def fetch_all_decisions():
     return Decision.objects.all()
 
 
-def get_decisions_with_filter(filters):
-    filtered_decisions = fetch_all_decisions()
+def filter_decisions(filters, decisions):
     for filter_key, filter_value in filters.items():
         # Ensures the field exist and the filter is not ill constructed
         Decision._meta.get_field(filter_key)
 
         # Filter the querySet
-        filtered_decisions = filtered_decisions.filter(**{filter_key: filter_value})
-    return filtered_decisions
+        decisions = decisions.filter(**{filter_key: filter_value})
+    return decisions
--- back/api/views.py
+++ back/api/views.py
@@ -2,17 +2,22 @@ import ast
 
 from django.http import JsonResponse
 
-from api.repository.decision_repository import get_decisions_with_filter
+from api.repository.decision_repository import fetch_all_decisions, filter_decisions
 from api.services.utils import transform_decision_list_to_json
 
 
 def get_decisions(request):
+    filters = request.GET.get('filters')
     input_search_bar = request.GET.get('input_search_bar')
-    # ToDo: input_search_bar match un des labels des jugements
-    print(input_search_bar)
 
-    filters = ast.literal_eval(request.GET.get('filters'))
-    filtered_decisions = get_decisions_with_filter(filters)
+    filtered_decisions = fetch_all_decisions()
+
+    if filters:
+        filters = ast.literal_eval(filters)
+        filtered_decisions = filter_decisions(filters, filtered_decisions)
+
+    if input_search_bar:
+        filtered_decisions = filtered_decisions.filter(**{"text__icontains": input_search_bar})
 
     return JsonResponse({
         "hits": transform_decision_list_to_json(filtered_decisions)
