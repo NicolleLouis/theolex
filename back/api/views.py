9df3fd8acaca800c0c4bfda6a518dfa613be23be
NicolleLouis <louisxnicolle@gmail.com>
2019-08-24 13:42:10 +0200

    get_all_decisions use filter param

--- back/api/views.py
+++ back/api/views.py
@@ -1,10 +1,21 @@
+import ast
+
 from django.http import JsonResponse
-from api.repository.decision_repository import fetch_decisions
+
+from api.repository.decision_repository import fetch_all_decisions, get_decisions_with_filter
+from api.services.utils import transform_decision_list_to_json
 
 
 def get_all_decisions(request):
+    input_search_bar = request.GET.get('input_search_bar')
+    # ToDo: input_search_bar match un des labels des jugements
+    print(input_search_bar)
+
+    filters = ast.literal_eval(request.GET.get('filters'))
+    filtered_decisions = get_decisions_with_filter(filters)
+
     return JsonResponse({
-        "hits": list(map(lambda decision: decision.to_json(), fetch_decisions()))
+        "hits": transform_decision_list_to_json(filtered_decisions)
     })
 
 
