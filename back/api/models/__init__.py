d8aaac114a746eaff31c201e6dd556816abe8c74
NicolleLouis <louisxnicolle@gmail.com>
2019-08-26 11:54:54 +0200

    Add authority in dataase

--- back/api/models/__init__.py
+++ back/api/models/__init__.py
@@ -1,1 +1,2 @@
+from .authority import *
 from .decision import *
--- back/api/models/decision.py
+++ back/api/models/decision.py
@@ -1,19 +1,39 @@
 from django.db import models
-from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
 
 class Decision(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.TextField(null=True)
-    text = models.TextField(null=True)
-    monetary_sanction = models.FloatField(null=True)
+    name = models.TextField(
+        null=True,
+        blank=True
+    )
+    text = models.TextField(
+        null=True,
+        blank=True
+    )
+    monetary_sanction = models.FloatField(
+        null=True,
+        blank=True
+    )
     # A supprimer quand on aura les modèles sous jacents? Ou au moins Enum et pas texte
-    type = models.TextField(null=True)
-    decision_date = models.DateTimeField(null=True)
+    type = models.TextField(
+        null=True,
+        blank=True
+    )
+    decision_date = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+    authority = models.ForeignKey(
+        'Authority',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL
+    )
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     def to_json(self):
         return {
@@ -22,24 +42,19 @@ class Decision(models.Model):
             'monetary_sanction': self.monetary_sanction,
             'type': self.type,
             'decision_date': self.decision_date,
+            'authority_name': self.authority.name if self.authority else None,
         }
 
 
-class DecisionResource(resources.ModelResource):
-    class Meta:
-        model = Decision
-
-    # Modify instance to customize
-    def before_save_instance(self, instance, using_transactions, dry_run):
-        pass
-
-
 class DecisionAdmin(ImportExportModelAdmin):
-    resource_class = DecisionResource
     list_display = (
         'name',
-        'text',
+        'get_authority_name',
         'monetary_sanction',
         'type',
         'decision_date',
     )
+
+    def get_authority_name(self, instance):
+        return instance.authority.name if instance.authority else None
+    get_authority_name.short_description = "authority"
