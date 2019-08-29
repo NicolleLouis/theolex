a02fbb9b0827c07849e34aae45ac5955bb3d213b
NicolleLouis <louisxnicolle@gmail.com>
2019-08-29 17:53:46 +0200

    Return violations in json decision plus command import

--- back/api/repository/decision_repository.py
+++ back/api/repository/decision_repository.py
@@ -1,21 +1,27 @@
 from api.models import Decision
 
 
-def fetch_all_decisions():
-    return Decision.objects.all()
+class DecisionRepository:
+    @staticmethod
+    def fetch_all_decisions():
+        return Decision.objects.all()
 
+    @staticmethod
+    def filter_decisions(filters, decisions):
+        for filter_key, filter_value in filters.items():
+            # Ensures the field exist and the filter is not ill constructed
+            Decision._meta.get_field(filter_key)
 
-def filter_decisions(filters, decisions):
-    for filter_key, filter_value in filters.items():
-        # Ensures the field exist and the filter is not ill constructed
-        Decision._meta.get_field(filter_key)
+            # Filter the querySet
+            decisions = decisions.filter(**{filter_key: filter_value})
+        return decisions
 
-        # Filter the querySet
-        decisions = decisions.filter(**{filter_key: filter_value})
-    return decisions
+    @staticmethod
+    def get_distinct_values_of_field(field_name):
+        raw_values = list(Decision.objects.values(field_name).distinct())
+        values = list(map(lambda raw_value: raw_value[field_name], raw_values))
+        return values
 
-
-def get_distinct_values_of_field(field_name):
-    raw_values = list(Decision.objects.values(field_name).distinct())
-    values = list(map(lambda raw_value: raw_value[field_name], raw_values))
-    return values
+    @staticmethod
+    def get_decision_by_name(name):
+        return Decision.objects.get(name=name)
--- back/api/views.py
+++ back/api/views.py
@@ -2,7 +2,7 @@ import ast
 
 from django.http import JsonResponse
 
-from api.repository.decision_repository import fetch_all_decisions, filter_decisions, get_distinct_values_of_field
+from api.repository.decision_repository import DecisionRepository
 from api.services.utils import transform_decision_list_to_json
 
 
@@ -10,11 +10,11 @@ def get_decisions(request):
     filters = request.GET.get('filters')
     input_search_bar = request.GET.get('input_search_bar')
 
-    filtered_decisions = fetch_all_decisions()
+    filtered_decisions = DecisionRepository.fetch_all_decisions()
 
     if filters:
         filters = ast.literal_eval(filters)
-        filtered_decisions = filter_decisions(filters, filtered_decisions)
+        filtered_decisions = DecisionRepository.filter_decisions(filters, filtered_decisions)
 
     if input_search_bar:
         filtered_decisions = filtered_decisions.filter(**{"text__icontains": input_search_bar})
@@ -26,7 +26,7 @@ def get_decisions(request):
 
 def get_filter_values(request):
     filter_label = request.GET.get('filter_label')
-    distinct_values_of_field = get_distinct_values_of_field(filter_label)
+    distinct_values_of_field = DecisionRepository.get_distinct_values_of_field(filter_label)
 
     return JsonResponse({
             "filter_label": filter_label,
