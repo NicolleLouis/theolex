f87fb75a042ba6e5c8e20d93bea632439658b142
jbng <johann.bertrand.ng@gmail.com>
2019-09-12 18:04:55 +0200

    Removed Violation filter, added Justice type filter

--- front/src/components/organisms/filters-section.js
+++ front/src/components/organisms/filters-section.js
@@ -6,21 +6,23 @@ const FiltersSection = ({filters, setFilters}) => {
 
   return (
     <Card>
-      <div className="form-group mb--5 mt--3">
+      <div className="form-group mb--4 mt--3">
         <div className="row">
           <Filter
-            id="filter-type"
-            label="type"
+            id="filter-justice-type"
+            label="Justice type"
+            name="justice_type"
             className="col-4 col-md-2"
-            value={filters.type}
+            value={filters.justice_type}
             filters={filters}
             setFilters={setFilters}
           />
           <Filter
-            id="filter-violation"
-            label="violation"
+            id="filter-type"
+            label="Type"
+            name="type"
             className="col-4 col-md-2"
-            value={filters.violation}
+            value={filters.type}
             filters={filters}
             setFilters={setFilters}
           />
