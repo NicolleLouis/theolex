628f46e991cda76d53651bd14ed47b35e04b9a23
jbng <johann.bertrand.ng@gmail.com>
2019-09-13 14:23:46 +0200

    Fixed search result display after 1rst search

--- front/src/components/organisms/result-section.js
+++ front/src/components/organisms/result-section.js
@@ -4,14 +4,20 @@ import ResultList from "../molecules/result-list";
 const ResultSection = ({ isError, isSearching, result }) => {
   return (
     <div className="ml-4">
-      {isError && <div>Something went wrong ...</div>}
-      {isSearching && <div>Searching ...</div>}
-      {result && result.hits && result.hits.length > 0 && (
-        <>
-          <div>{result.hits.length} result(s)</div>
-          <br />
-          <ResultList result={result} />
-        </>
+      {isError ? (
+        <div>Something went wrong ...</div>
+      ) : isSearching ? (
+        <div>Searching ...</div>
+      ) : (
+        result &&
+        result.hits &&
+        result.hits.length > 0 && (
+          <>
+            <div>{result.hits.length} result(s)</div>
+            <br />
+            <ResultList result={result} />
+          </>
+        )
       )}
     </div>
   );
