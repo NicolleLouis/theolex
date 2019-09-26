f8f31cf645e47227e4b81c0c64d21a752ef3c140
jbng <johann.bertrand.ng@gmail.com>
2019-09-26 12:47:38 +0200

    Bugfix basket checkbox algorithm

--- front/src/components/molecules/result-list.js
+++ front/src/components/molecules/result-list.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import ResultListedEntry from "./result-listed-entry";
+import ApplicationContext from "../../config/application-context";
 
 const ResultList = ({ result }) => {
+  const { basketCxt } = useContext(ApplicationContext);
+  const { basket } = basketCxt;
+
   return (
     <>
       {result.hits.length > 0 &&
-        result.hits.map((entry, index) => (
-          <ResultListedEntry key={index} content={entry} />
-        ))}
+        result.hits.map((entry, index) => {
+          let isChecked = false;
+          if (
+            basket &&
+            basket.decisions &&
+            basket.decisions.length > 0 &&
+            basket.decisions.find(decision => decision.id === entry.id)
+          ) {
+            isChecked = true;
+          }
+
+          return (
+            <ResultListedEntry
+              key={index}
+              content={entry}
+              isChecked={isChecked}
+            />
+          );
+        })}
     </>
   );
 };
