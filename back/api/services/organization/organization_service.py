987eed631dbd3d3e26f5395e59688a22991a8269
NicolleLouis <louisxnicolle@gmail.com>
2019-10-02 14:32:52 +0200

    Improve api get amount paid by company

--- back/api/services/organization/organization_service.py
+++ back/api/services/organization/organization_service.py
@@ -1,4 +1,5 @@
 from api.repository.organization_repository import OrganizationRepository
+from api.services.formatter import FormatterService
 
 
 class OrganizationService:
@@ -8,9 +9,12 @@ class OrganizationService:
         organizations = OrganizationRepository.get_all_organizations()
 
         for organization in organizations:
+            amount_paid = OrganizationRepository.get_total_amount_paid_by_organization(organization)
             organizations_with_total_amount_paid.append({
                 "company": organization.name,
-                "amount_paid": OrganizationRepository.get_total_amount_paid_by_organization(organization)
+                "company_type": organization.company_type,
+                "amount_paid": amount_paid,
+                "amount_paid_formatted": FormatterService.format_monetary_amount(amount_paid)
             })
 
         return organizations_with_total_amount_paid
